d3cef23514fe4f967fce2009531352b9add47e28
Remi A. <135215524+remabd@users.noreply.github.com>
2026-01-14 19:01:57 +0100

    feat-post: Ajout du controller post et fin du manager (#12)
    * feat-post: Ajout du controller post et fin du manager
    
    * fix: ajustement des imports
    
    * fix: ajustement de la bdd
    
    * feat: fix de la récupération des posts
    
    * feat: ajoujt des return type
    
    * feat: ajout update Post
    
    * fix: ajout test connexion

---
+++ view/createPost.php
@@ -0,0 +1,36 @@
+<form method="POST" action="index.php?ctrl=post&action=doCreate">
+
+    <?php if (!empty($errors)): ?>
+        <div class="error">
+            <?= htmlspecialchars($errors) ?>
+        </div>
+    <?php endif; ?>
+
+    <div>
+        <label for="title">Title</label><br>
+        <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            minlength="3"
+        >
+    </div>
+
+    <br>
+
+    <div>
+        <label for="content">Content</label><br>
+        <textarea
+            id="content"
+            name="content"
+            rows="6"
+            required
+        ></textarea>
+    </div>
+
+    <br>
+
+    <button type="submit">Publier</button>
+
+</form>
