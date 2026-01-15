ab3d02dddc9580cdbd0a7dec780fc8b919c07ecd
Pierre <nouhet.pierre@gmail.com>
2026-01-15 22:12:37 +0100

    modifs sur la vue profile, affichage des posts de l'utilisateur dans son profil

--- view/profile.php
+++ view/profile.php
@@ -10,19 +10,25 @@
     <a href="index.php?ctrl=user&action=delete&id=<?= $user->getId() ?>">Supprimer son profil</a>
 </div>
 
-<h2>Posts</h2>
-<?php if (!empty($posts)): ?>
-    <ul>
-        <?php foreach ($posts as $post): ?>
-            <li>
-                <?= htmlspecialchars($post["title"]) ?> - <?= htmlspecialchars(
-     $post["date"],
- ) ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php else: ?>
-    <p>No posts yet.</p>
-<?php endif; ?>
+<div class="user-posts">
+    <h2>Posts crées</h2>
+    <?php if (!empty($posts)): ?>
+        <ul>
+            <?php foreach ($posts as $post): ?>
+                <li>
+                    <?= htmlspecialchars($post["title"]) ?>
+                    <span>- crée le </span>
+                    <?= htmlspecialchars($post["createdAt"]) ?>
+                    <span>-</span>
+                    <a href="index.php?ctrl=post&action=update&id=<?= htmlspecialchars($post["_id"]) ?>">
+                        <button type="button">Voir le post</button>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>Vous n'avez pas encore de posts.</p>
+    <?php endif; ?>
+</div>
 
 </section>
